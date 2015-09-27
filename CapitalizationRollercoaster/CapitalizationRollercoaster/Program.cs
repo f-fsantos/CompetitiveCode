dae1d7c8d48fedc21c41e460384b3c9c0df3c319
XGSleepWalker <frederico.f.santos@tecnico.ulisboa.pt>
2015-09-27 05:04:24 +0100

    Adding Capitalization Rollercoaster Solution

---
+++ CapitalizationRollercoaster/CapitalizationRollercoaster/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.IO;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        bool ToUp = true;
+        int index = 0;
+        string print = "";
+        using (StreamReader reader = File.OpenText(args[0]))
+            while (!reader.EndOfStream)
+            {
+                string line = reader.ReadLine();
+                if (null == line)
+                    continue;
+                while (index < line.Length)
+                {
+                    if (char.IsLetter(line[index]))
+                    {
+                        if (ToUp)
+                        {
+                            print += char.ToUpper(line[index]);
+                            ToUp = false;
+                        }
+                        else
+                        {
+                            print += char.ToLower(line[index]);
+                            ToUp = true;
+                        }
+                    }
+                    else
+                        print += line[index];
+                    index++;
+                }
+                index = 0;
+                System.Console.WriteLine(print);
+                print = "";
+                ToUp = true;
+
+            }
+    }
+}
