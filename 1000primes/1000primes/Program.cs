188a670b55aeaf83271e5e14730e9d0288fe745d
XGSleepWalker <frederico.f.santos@tecnico.ulisboa.pt>
2015-09-27 03:29:40 +0100

    First Commit - Adding Sum of 1000 Primes Solution

---
+++ 1000primes/1000primes/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int i = 3;
+        int sum = 2;
+        int index = 0;
+        bool prime = true;
+        List<int> array = new List<int>();
+        array.Add(2);
+        while (array.Count < 1000)
+        {
+            while (index < array.Count && prime == true)
+            {
+                if(i % array[index] == 0)
+                    prime = false;
+                index++;
+            }
+            if (prime == true)
+            {
+                array.Add(i);
+                sum += i;
+            }
+            i++;
+            prime = true;
+            index = 0;
+        }
+        Console.WriteLine(sum);
+    }
+}
