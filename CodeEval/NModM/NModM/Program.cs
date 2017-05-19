48028598386017ead07eadef36a46b97a6ea8cab
Frederico Santos <frederico.f.santos@tecnico.ulisboa.pt>
2017-05-19 04:10:09 +0100

    NModM Solution for CodeEval

---
+++ CodeEval/NModM/NModM/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using System;
+
+class Program {
+    static void Main(string[] args) {
+        using (StreamReader reader = File.OpenText(args[0]))
+            while (!reader.EndOfStream) {
+                string line = reader.ReadLine();
+                if (null == line)
+                    continue;
+                int n = Int32.Parse(line.Split(',')[0]);
+                int m = Int32.Parse(line.Split(',')[1]);
+                int result;
+                int tempResult = n / m;
+                result = n - (m * tempResult);
+                Console.WriteLine(result);
+            }
+    }
+}
