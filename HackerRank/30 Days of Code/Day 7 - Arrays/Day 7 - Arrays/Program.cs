c14b2c8a60a362d79266c6292ecbbc15984d0301
Frederico Santos <frederico.f.santos@tecnico.ulisboa.pt>
2017-05-25 00:01:34 +0100

    HackerRank 30 days of Code - Day 7

---
+++ HackerRank/30 Days of Code/Day 7 - Arrays/Day 7 - Arrays/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_7___Arrays {
+    class Program {
+        static void Main(string[] args) {
+            int n = Convert.ToInt32(Console.ReadLine());
+            string[] arr_temp = Console.ReadLine().Split(' ');
+            int[] arr = Array.ConvertAll(arr_temp, Int32.Parse);
+            string output = "";
+            Array.Reverse(arr);
+            for (int i = 0; i < n; i++) {
+                output += arr[i] + " ";
+            }
+            output = output.Trim();
+            Console.WriteLine(output);
+        }
+    }
+}
