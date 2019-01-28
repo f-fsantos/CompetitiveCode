1b68bca3b947f2590b43575bbf2f13cd6544b769
Frederico Santos <frederico.santos@farfetch.com>
2019-01-28 01:46:07 +0100

    Leetcode

---
+++ Leetcode/771.Jewels_And_Stones/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+namespace _771.Jewels_And_Stones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        // runtime: 88ms - faster than 87.24%
+        public static int numJewelsInStonesForEach(string J, string S) 
+        {
+            int counter = 0;
+            foreach(char letter in S)
+            {
+                if(J.Contains(letter))
+                counter++;
+            }
+            return counter;
+        }
+        
+        // runtime: 104ms - faster than 26.07% 
+        public static int JewelsInStonesLinq(string J, string S)
+        {
+            return S.Where(x => J.Contains(x)).Count();
+        }
+
+        // runtime: 84ms - faster than 96.44%
+        public static int JewelsInStonesFor(string J, string S){
+            int counter = 0;
+
+            for (int i = 0; i < S.Length; i++)
+            {
+                for(int k = 0; k < J.Length; k++)
+                { 
+                    if(S[i] == J[k]) 
+                    {
+                        counter++;
+                    } // no brackets - extra 4ms on execution - god bless branch optimization
+                }
+                
+            }
+            return counter;
+        }
+    }
+}
---
+++ Leetcode/929.Unique_Email_Addresses/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _929.Unique_Email_Addresses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine(NumUniqueEmailsHashSet(new string[]
+            {
+                "test.email+alex@leetcode.com",
+                "test.e.mail+bob.cathy@leetcode.com",
+                "testemail+david@lee.tcode.com"
+            }));
+        }
+
+        // runtime: 128ms - faster than 89.32%
+        public static int NumUniqueEmailsClass(string[] emails)
+        {
+            int counter = 0;
+            List<Email> emailList = new List<Email>();
+            foreach (string email in emails)
+            {
+                Email address = new Email(email);
+                if (!emailList.Any(x => x.Local == address.Local && x.Domain == address.Domain)){
+                    counter++;
+                    emailList.Add(address);
+                }
+            }
+            return counter;
+        }   
+
+        // runtime: 128ms - faster than 89.32%
+        public static int NumUniqueEmailsHashSet(string[] emails)
+        {
+            HashSet<string> hashSet = new HashSet<string>(emails.Count());
+            int counter = 0;
+            foreach(string email in emails)
+            {
+                bool ignore = false;
+                StringBuilder builder = new StringBuilder();
+                for(int i = 0; i < email.Length; i++)
+                {
+                    char letter = email[i];
+                    if(letter == '@')
+                    {
+                        builder.Append(email.Substring(i));
+                        break;
+                    }
+                    if(letter == '+')
+                    {
+                        ignore = true;
+                        continue;
+                    }
+                    if(letter == '.')
+                    {
+                        continue;
+                    }
+                    if (!ignore)
+                    {
+                        builder.Append(letter);
+                    }
+                }
+                string address = builder.ToString();
+                if(!hashSet.Contains(address))
+                {
+                    hashSet.Add(address);
+                    counter++;
+                }
+            }
+            return counter;
+        }
+    }
+    class Email {
+        public string Domain { get; set; }
+
+        public string Local { get; set; }
+
+        public Email(string email)
+        {
+            string[] splitEmail = email.Split('@');
+            Domain = splitEmail[1];
+
+            Local = splitEmail[0].Substring(0, splitEmail[0].IndexOf('+')).Replace(".", "");
+        }
+    }
+}
