ab6e7a9420da05116acb13639eb5704d4707fcde
Frederico Santos <frederico.santos@farfetch.com>
2019-12-13 15:47:09 +0000

    Custom problem and Forming a Magic Square

---
+++ Custom/DoubleLinkedList/Program.cs
@@ -0,0 +1,115 @@
+﻿namespace DoubleLinkedList
+{
+    using System;
+    public class Program
+    {
+        private static Node Root = null;
+        public static void Main()
+        {
+            var random = new Random();
+            var limit = 100;
+            for (var i = 0; i < limit; i++)
+            {
+                Root = insertInSortedPosition(Root, random.Next(0, 1000));
+            }
+            ValidateList(limit);
+        }
+        private static Node insertInSortedPosition(Node root, int value)
+        {
+            var newNode = new Node
+            {
+                Next = null,
+                Previous = null,
+                Value = value
+            };
+
+            Node curNode = root;
+            
+            if(root == null)
+            {
+                return newNode;
+            }
+            if (value < root.Value)
+            {
+                BeforeNode(root, newNode);
+                return newNode;
+            }
+
+            while (curNode.Next != null)
+            {
+                if (value < curNode.Value)
+                {
+                    BeforeNode(curNode, newNode);
+                    return root;
+                }
+                curNode = curNode.Next;   
+            }
+            if (curNode.Value < newNode.Value)
+            {
+                curNode.Next = newNode;
+                newNode.Previous = curNode;
+            }
+            else 
+            {
+                newNode.Next = curNode;
+                newNode.Previous = curNode.Previous;
+                curNode.Previous.Next = newNode;
+                curNode.Previous = newNode;
+            }
+
+            return root;
+        }
+
+        private static void BeforeNode(Node oldNode, Node newNode)
+        {
+            if (oldNode.Previous != null)
+            {
+                oldNode.Previous.Next = newNode;
+                newNode.Previous = oldNode.Previous;
+            }
+            oldNode.Previous = newNode;
+            newNode.Next = oldNode;
+        }
+
+        private static void ValidateList(int exceptedNumElments)
+        {
+            var node = Root;
+            var valid = true;
+            var numElements = 0;
+            while (node != null)
+            {
+                numElements++;
+                if (numElements == 1 && (node.Previous != null || node.Next == null))
+                {
+                    valid = false;
+                    break;
+                }
+                if (numElements == exceptedNumElments && (node.Next != null || node.Previous == null))
+                {
+                    valid = false;
+                    break;
+                }
+                if ((node.Next != null && node.Next.Value < node.Value) || (node.Previous != null && node.Previous.Value > node.Value))
+                {
+                    valid = false;
+                    break;
+                }
+                node = node.Next;
+            }
+            if (valid && numElements == exceptedNumElments)
+            {
+                Console.WriteLine("VALID");
+            }
+            else
+            {
+                Console.WriteLine("INVALID");
+            }
+        }
+        class Node
+        {
+            public Node Previous { get; set; }
+            public Node Next { get; set; }
+            public int Value { get; set; }
+        }
+    }
+}
---
+++ HackerRank/Forming a Magic Square/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Forming_a_Magic_Square
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        static int formingMagicSquare(int[][] s)
+        {
+            int[] unusedNumbers = new int[]{1,2,3,4,5,6,7,8,9};
+            for (int i = 0; i < 2; i++)
+            {
+                                
+            }
+            
+            return 0;
+        }
+        static void RowCheck(int[] a, Set set)
+        {
+
+        }
+    }
+    class Set 
+    {
+        int a {get; set;}
+        int b {get; set;}
+        int c {get; set;}
+
+    }
+}
